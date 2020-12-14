ba2e776cafcf287fc84ef7492de9a80cae2e94e3
mayralgr <ml17ft@gmail.com>
2020-12-14 15:14:15 -0600

    Add module imports and exports

---
+++ public/js/sessionRepository.js
@@ -0,0 +1,21 @@
+const sessionURL = 'sessions.json'
+
+let sessionList = [];
+
+// export default function getSessions()
+function getSessions(){
+    return new Promise(function(resolve, reject){
+      var oReq = new XMLHttpRequest();
+      oReq.onload = function (e) {
+        sessionList = e.target.response
+        resolve(sessionList);
+      };
+      oReq.open('GET', sessionURL, true);
+      oReq.responseType = 'json';
+      oReq.send();
+    })
+  }
+
+//export { getSessions };
+export { getSessions as default, sessionURL}
+export { sessionTemplate } from './template.js'
